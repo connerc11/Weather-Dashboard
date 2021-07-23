124f3271704e7118eea3137fe63d08c3002255cc
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-07-23 14:25:54 -0400

    updating variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 var cityLocations = []
- 
+
+var searchHeadingEl = document.querySelector("#search-city");
+var inputCityEl = document.querySelector("#city-name");
+var previousSearchedEl = document.querySelector("#previous-searched");
+var cityChoiceEl = document.querySelector("#selected-city");
+var weeklyForecastEl = document.querySelector("#weekly-forecast");
+var weatherForecast = document.querySelector("#weather");
+var currentWeather = document.getElementById("#current-weather");
+
+
+
+
