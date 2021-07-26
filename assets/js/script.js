266d8631662ed134ea45656faa08b01019f78cd6
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-07-26 18:52:50 -0400

    updated uvi

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ var cityChoiceEl = document.querySelector("#selected-city");
 var weeklyForecastEl = document.querySelector("#weekly-forecast");
 var weatherForecast = document.querySelector("#weather");
 var currentWeather = document.getElementById("#current-weather");
+var card = $("<div>").addClass("card");
+var cardBody = $("<div>").addClass("card-body");
 
 var apiKey = "d1b5b46c7ffa1a821d537bdfb121c6be";
 var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=';
@@ -37,14 +39,12 @@ var retreiveCityWeather = function (event) {
             }
             var lat = data.coord.lat
             var lon = data.coord.lon
-            var card = $("<div>").addClass("card");
-            var cardBody = $("<div>").addClass("card-body");
             var cardTitle = $("<h4>").addClass("card-title").text(data.name);
             var cardTemp = $("<p>").addClass("card-text currentTemp").text("Temperature~" + data.main.temp + " °F");
             var cardHumidity =$("<p>").addClass("card-text currentHumid").text("Humidity~" + data.main.humidity + " %");
             var cardWind = $("<p>").addClass("card-text currentWind").text("Wind Speed~" + data.wind.speed + " MPH");
-            var cardIndex = $("<p>").addClass("card-text currentIndex").text("UVI~" + data.uvi);
-           $("#main-weather").append(card.append(cardBody.append(cardTitle, cardTemp, cardHumidity, cardWind, cardIndex)));
+
+            $("#main-weather").append(card.append(cardBody.append(cardTitle, cardTemp, cardHumidity, cardWind)));
             secondFunction(lat, lon)
         })
 
@@ -54,11 +54,14 @@ var retreiveCityWeather = function (event) {
 var secondFunction = function (lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`)
         .then(function (response) {
-            return response.json();
+            return response.json()
         })
         .then(function (data) {
+            console.log(data)
+            var cardIndex = $("<p>").addClass("card-text currentIndex").text("UV~" + data.current.uvi)
+            $("#main-weather").append(card.append(cardBody.append(cardIndex)));
             for(var i = 1; i < 6; i++){
-                console.log(data.daily[i]);
+                // console.log(data.daily[i]);
                 var dateEl = moment.unix(data.daily[i].dt).format("MMM Do")
                 console.log(dateEl)
             }
