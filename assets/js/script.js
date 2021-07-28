f956c44a5ca9fa1bb5381ba04e5addb529984bf9
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-07-28 17:19:50 -0400

    updated heading icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,9 +35,9 @@ var retreiveCityWeather = function (event) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${storedInfo}&units=imperial&appid=${apiKey}`)
         .then(function (response) {
             return response.json();
-            
-            
-            
+
+
+
         })
         .then(function (data) {
             console.log(data);
@@ -48,11 +48,12 @@ var retreiveCityWeather = function (event) {
             var lat = data.coord.lat
             var lon = data.coord.lon
             var cardTitle = $("<h4>").addClass("card-title").text(data.name)
+            var cardIcon = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
             var cardTemp = $("<p>").addClass("card-text currentTemp").text("Temperature~" + data.main.temp + " °F");
             var cardHumidity = $("<p>").addClass("card-text currentHumid").text("Humidity~" + data.main.humidity + " %");
             var cardWind = $("<p>").addClass("card-text currentWind").text("Wind Speed~" + data.wind.speed + " MPH");
 
-            $("#main-weather").append(card.append(cardBody.append(cardTitle, cardTemp, cardHumidity, cardWind)));
+            $("#main-weather").append(card.append(cardBody.append(cardTitle, cardIcon, cardTemp, cardHumidity, cardWind)));
             secondFunction(lat, lon)
         })
 
@@ -82,86 +83,72 @@ var secondFunction = function (lat, lon) {
             thirdFunction(lat, lon)
 
 
-            
-        //     for (var i = 1; i < 6; i++) {
-        //         console.log(data);
-        //         var dateEl = moment.unix(data.daily[i]).format("MMM Do")
-                
-
-        //         var cardTitle1 = $("<h4>").addClass("card-title1").text(data.daily[1].name)
-        //         var cardTemp1 = $("<p>").addClass("card-text currentTemp1").text("Temperature~" + data.daily[1].temp.day + " °F");
-        //         var cardHumidity1 = $("<p>").addClass("card-text currentHumid1").text("Humidity~" + data.daily[1].humidity + " %");
-        //         var cardWind1 = $("<p>").addClass("card-text currentWind1").text("Wind Speed~" + data.daily[1].wind_speed + " MPH");
-        //         $("#weather-city").append(card1.append(cardBody1.append(cardTitle1, cardTemp1, cardHumidity1, cardWind1)));
-                
-        //   //5
-        //     }
         })
-        
-        
-        var thirdFunction = function (lat, lon) {
-            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
-                .then(function (response) {
-                    return response.json()
-                    })
-                    .then(function (response) {
-                        console.log(fetch)
-                        console.log(response)
-                   var day1 = moment(response.list[1].dt_txt).format("MMM Do")
-                   console.log(moment(response.list[1].dt_txt).format("MMM Do"))
-                   
-                   $(".day1-data-date").html("<h5>" + day1 + "</h5>");
-                   $(".day1-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[1].weather[0].icon + ".png>")
-                   $(".day1-data-humidity").text("Humidity: " + response.list[1].main.humidity + "%");
-                   $(".day1-data-temp").text("Temperature : " + response.list[1].main.temp + " °F");
-                   $(".day1-data-wind").text("Wind : " + response.list[1].wind.speed + " MPH");
-
-                   var day2 = moment(response.list[9].dt_txt).format("MMM Do")
-                   console.log(moment(response.list[9].dt_txt).format("MMM Do"))
-                   
-                   $(".day2-data-date").html("<h5>" + day2 + "</h5>");
-                   $(".day2-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[9].weather[0].icon + ".png>")
-                   $(".day2-data-humidity").text("Humidity: " + response.list[9].main.humidity + "%");
-                   $(".day2-data-temp").text("Temperature : " + response.list[9].main.temp + " °F");
-                   $(".day2-data-wind").text("Wind : " + response.list[9].wind.speed + " MPH");
-
-                   var day3 = moment(response.list[17].dt_txt).format("MMM Do")
-                   console.log(moment(response.list[17].dt_txt).format("MMM Do"))
-                   
-                   $(".day3-data-date").html("<h5>" + day3 + "</h5>");
-                   $(".day3-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[17].weather[0].icon + ".png>")
-                   $(".day3-data-humidity").text("Humidity: " + response.list[17].main.humidity + "%");
-                   $(".day3-data-temp").text("Temperature : " + response.list[17].main.temp + " °F");
-                   $(".day3-data-wind").text("Wind : " + response.list[17].wind.speed + " MPH");
-
-                   var day4 = moment(response.list[25].dt_txt).format("MMM Do")
-                   console.log(moment(response.list[25].dt_txt).format("MMM Do"))
-                   
-                   $(".day4-data-date").html("<h5>" + day4 + "</h5>");
-                   $(".day4-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[25].weather[0].icon + ".png>")
-                   $(".day4-data-humidity").text("Humidity: " + response.list[25].main.humidity + "%");
-                   $(".day4-data-temp").text("Temperature : " + response.list[25].main.temp + " °F");
-                   $(".day4-data-wind").text("Wind : " + response.list[25].wind.speed + " MPH");
-
-                   var day5 = moment(response.list[33].dt_txt).format("MMM Do")
-                   console.log(moment(response.list[33].dt_txt).format("MMM Do"))
-                   
-                   $(".day5-data-date").html("<h5>" + day5 + "</h5>");
-                   $(".day5-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[25].weather[0].icon + ".png>")
-                   $(".day5-data-humidity").text("Humidity: " + response.list[33].main.humidity + "%");
-                   $(".day5-data-temp").text("Temperature : " + response.list[33].main.temp + " °F");
-                   $(".day5-data-wind").text("Wind : " + response.list[33].wind.speed + " MPH");
 
 
+    var thirdFunction = function (lat, lon) {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (response) {
+                console.log(fetch)
+                console.log(response)
+                var day1 = moment(response.list[1].dt_txt).format("MMM Do")
+                console.log(moment(response.list[1].dt_txt).format("MMM Do"))
+
+                $(".day1-data-date").html("<h5>" + day1 + "</h5>");
+                $(".day1-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[1].weather[0].icon + ".png>")
+                $(".day1-data-humidity").text("Humidity: " + response.list[1].main.humidity + "%");
+                $(".day1-data-temp").text("Temperature : " + response.list[1].main.temp + " °F");
+                $(".day1-data-wind").text("Wind : " + response.list[1].wind.speed + " MPH");
+
+                var day2 = moment(response.list[9].dt_txt).format("MMM Do")
+                console.log(moment(response.list[9].dt_txt).format("MMM Do"))
+
+                $(".day2-data-date").html("<h5>" + day2 + "</h5>");
+                $(".day2-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[9].weather[0].icon + ".png>")
+                $(".day2-data-humidity").text("Humidity: " + response.list[9].main.humidity + "%");
+                $(".day2-data-temp").text("Temperature : " + response.list[9].main.temp + " °F");
+                $(".day2-data-wind").text("Wind : " + response.list[9].wind.speed + " MPH");
+
+                var day3 = moment(response.list[17].dt_txt).format("MMM Do")
+                console.log(moment(response.list[17].dt_txt).format("MMM Do"))
+
+                $(".day3-data-date").html("<h5>" + day3 + "</h5>");
+                $(".day3-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[17].weather[0].icon + ".png>")
+                $(".day3-data-humidity").text("Humidity: " + response.list[17].main.humidity + "%");
+                $(".day3-data-temp").text("Temperature : " + response.list[17].main.temp + " °F");
+                $(".day3-data-wind").text("Wind : " + response.list[17].wind.speed + " MPH");
+
+                var day4 = moment(response.list[25].dt_txt).format("MMM Do")
+                console.log(moment(response.list[25].dt_txt).format("MMM Do"))
+
+                $(".day4-data-date").html("<h5>" + day4 + "</h5>");
+                $(".day4-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[25].weather[0].icon + ".png>")
+                $(".day4-data-humidity").text("Humidity: " + response.list[25].main.humidity + "%");
+                $(".day4-data-temp").text("Temperature : " + response.list[25].main.temp + " °F");
+                $(".day4-data-wind").text("Wind : " + response.list[25].wind.speed + " MPH");
+
+                var day5 = moment(response.list[33].dt_txt).format("MMM Do")
+                console.log(moment(response.list[33].dt_txt).format("MMM Do"))
+
+                $(".day5-data-date").html("<h5>" + day5 + "</h5>");
+                $(".day5-data-icon").html("<img src=https://openweathermap.org/img/w/" + response.list[25].weather[0].icon + ".png>")
+                $(".day5-data-humidity").text("Humidity: " + response.list[33].main.humidity + "%");
+                $(".day5-data-temp").text("Temperature : " + response.list[33].main.temp + " °F");
+                $(".day5-data-wind").text("Wind : " + response.list[33].wind.speed + " MPH");
 
 
-                   
-                    })
 
-            
 
-                
-        }
+
+            })
+
+
+
+
+    }
 }
 
 
